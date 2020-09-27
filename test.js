4e6ff8ca67aff919d490d1f3ae48c87965176702
superjojo140 <plooploe@web.de>
2020-09-27 13:11:53 +0200

    Add cool output

--- test.js
+++ test.js
@@ -2,4 +2,5 @@ console.log("Let's test yeah");
 console.log(`Env was ${process.env.mname}`)
 if(process.env.mname == "Johannes"){
     console.log("Name was Johannes");
-}   
+}  
+console.log("This was be deployed now") 
